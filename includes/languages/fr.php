1a0e9e6f00b71a3bff51a8c45d8c501f05314b40
Cornea Ionut <ionut.cornea@pitechnologies.ro>
2022-12-13 02:06:59 +0200

    Multi-language feature.

---
+++ includes/languages/fr.php
@@ -0,0 +1,6 @@
+<?php
+
+const _REGISTER = "Registration";
+const _USERNAME = "Nom d'user";
+const _EMAIL = "L'email";
+const _PASSWORD = "Mot du pass";
--- registration.php
+++ registration.php
@@ -1,9 +1,28 @@
 <?php include "includes/db.php"; ?>
 <?php include "includes/header.php"; ?>
+<!-- Navigation -->
+<?php include "includes/navigation.php"; ?>
 <?php require_once "./admin/functions.php"; ?>
 
+<?php
+// Setting Language Variables
+//session_start();
+if (isset($_GET['lang']) && !empty($_GET['lang'])) {
+    $_SESSION['lang'] = $_GET['lang'];
+
+    if (isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']) {
+        echo "<script type='text/javascript'> location.reload(); </script>";
+    }
+}
+if (isset($_SESSION['lang'])) {
+    include "includes/languages/".$_SESSION['lang'].".php";
+} else {
+    include "includes/languages/en.php";
+}
+?>
 
 <?php
+// AUTHENTICATION
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     $username = trim(escape($_POST['username']));
@@ -52,43 +71,48 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 }
 ?>
 
-<!-- Navigation -->
-<?php include "includes/navigation.php"; ?>
-
-
 <!-- Page Content -->
 <div class="container">
+<!--Multi-Language Implementation-->
+    <form method="get" class="navbar-form navbar-right" action="" id="language_form">
+        <div class="form-group">
+            <select name="lang" class="form-control" onchange="changeLanguage()">
+                <option value="en" <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'en') { echo "selected"; } ?> >English</option>
+                <option value="fr" <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'fr') { echo "selected"; } ?> >French</option>
+            </select>
+        </div>
+    </form>
 
     <section id="login">
         <div class="container">
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
-                        <h1>Register</h1>
+                        <h1><?php echo _REGISTER; ?></h1>
                         <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                             <div class="form-group">
                                 <label for="username" class="sr-only">username</label>
                                 <input type="text" name="username" id="username" class="form-control" autocomplete="on"
                                        value="<?php echo $username ?? '' ?>"
-                                       placeholder="Enter Desired Username">
+                                       placeholder="<?php echo _USERNAME; ?> ">
                                 <p><?php echo isset($error['username']) ? $error['username'] : '' ?></p>
                             </div>
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
                                 <input type="email" name="email" id="email" class="form-control" autocomplete="on"
                                        value="<?php echo isset($email) ? $email : '' ?>"
-                                       placeholder="somebody@example.com">
+                                       placeholder="<?php echo _EMAIL; ?> ">
                                 <p><?php echo isset($error['email']) ? $error['email'] : '' ?></p>
                             </div>
                             <div class="form-group">
                                 <label for="password" class="sr-only">Password</label>
                                 <input type="password" name="password" id="key" class="form-control"
-                                       placeholder="Password">
+                                       placeholder="<?php echo _PASSWORD; ?> ">
                                 <p><?php echo isset($error['password']) ? $error['password'] : '' ?></p>
                             </div>
 
-                            <input type="submit" name="register" id="btn-login" class="btn btn-custom btn-lg btn-block"
-                                   value="Register">
+                            <input type="submit" name="register" id="btn-login" class="btn btn-primary btn-lg btn-block"
+                                   value="<?php echo _REGISTER; ?> ">
                         </form>
 
                     </div>
@@ -99,4 +123,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 
     <hr>
+    <script>
+        function changeLanguage() {
+            document.getElementById('language_form').submit();
+        }
+    </script>
+
     <?php include "includes/footer.php"; ?>
